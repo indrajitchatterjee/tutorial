7877c01be67ac1020706bad30ea1493d6ba9efc8
Indrajit <indrajit.chatterjee2@gmail.com>
2024-12-27 11:54:49 +0530

    Chapter2 Added

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Chapter1 from "./sec1/ch1.jsx";
+import Home from './Home';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <Chapter1 />
+      <Home />
   </StrictMode>,
 )
---
+++ src/sec1/ch2.jsx
@@ -0,0 +1,22 @@
+export default function Chapter2(){
+    return(
+        <>
+            <h1>Techniques of Problem Solving</h1>
+            <p>
+            In this tutorial, we will learn about:
+            <ul>
+                <li>Algorithms</li>
+                <li>Flowcharting</li>
+                <li>Decission Table</li>
+                <li>Programming Methodologies viz. Top-Down and Buttom-Up approach</li>
+                <li>Structured Programming Concept</li>
+            </ul>
+            </p>
+            <h2>Algorithm</h2>
+            <p>
+                <b>Algorithm</b> is a finite sequence of well defined, computer implementable,
+                instructions typically to solve a class of problems or to perform a computation.
+            </p>
+        </>
+    );
+}
